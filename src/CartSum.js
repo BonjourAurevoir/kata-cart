6c0880bc5b2122e4e1f2820d0a3e160c684ecc5c
Guillaume Denis <gdenis@simplon.co>
2015-11-30 17:25:43 +0100

    soluce avec underscore

--- src/CartSum.js
+++ src/CartSum.js
@@ -1,3 +1,28 @@
 function cartSum(cart) {
-  
+  // Sans la librairie underscorejs les deux premières conditions s'écrivent ainsi
+  /*if(!Array.isArray(cart) && typeof cart !== 'object') {
+    return null;
+  } else if (( Array.isArray(cart) && cart.length === 0) || Object.keys(cart).length === 0) {
+    return 0;
+  }*/
+
+  // mais avec underscorejs c'est plus concis
+  if (!_.isArray(cart) && !_.isObject(cart)) {
+    return null;
+  } else if (_.isEmpty(cart)) {
+    return 0;
+  } else if (_.isArray(cart)) {
+    var total = 0;
+    cart.forEach(function(item) {
+      var q = _.isNumber(item.q) ? item.q : 1;
+      total += item.price * q;
+    });
+    return total;
+  } else {
+    var q = 1;
+    if (typeof cart.q !== "undefined") {
+      q = cart.q;
+    }
+    return cart.price * q;
+  }
 }
